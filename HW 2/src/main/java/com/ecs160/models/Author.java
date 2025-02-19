8d6fb69bcc9559561c152d53e84ecfccdca65a1c
patrickhcnguyen <patricknguyen714@gmail.com>
2025-02-19 02:06:08 -0800

    workflow

---
+++ HW 2/src/main/java/com/ecs160/models/Author.java
@@ -0,0 +1,30 @@
+package com.ecs160.models;
+
+public class Author {
+    // vals
+
+    private String did;
+    private String handle;
+    private String displayName;
+    private String name;
+
+    // getters and setters
+    public String getDid() { return did; }
+    public void setDid(String did) { this.did = did; }
+
+    public String getHandle() { return handle; }
+    public void setHandle(String handle) { this.handle = handle; }
+
+    public String getDisplayName() { return displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    //HIIII
+}
---
+++ HW 2/src/main/java/com/ecs160/models/Post.java
@@ -0,0 +1,73 @@
+package com.ecs160.models;
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class Post {
+    private Integer postId;
+    private String postContent;
+    private List<Post> replies;
+
+    private String uri;
+    private String cid;
+    private Author author;
+    private Record record;
+    private int replyCount;
+    private int repostCount;
+    private int likeCount;
+    private int quoteCount;
+    private String indexedAt;
+    private LocalDateTime timestamp;
+
+    // getters and setters
+    public Integer getPostId() { return postId; }
+    public void setPostId(Integer postId) { this.postId = postId; }
+
+    public String getPostContent() { return postContent; }
+    public void setPostContent(String postContent) { this.postContent = postContent; }
+
+    public String getUri() { return uri; }
+    public void setUri(String uri) { this.uri = uri; }
+
+    public String getCid() { return cid; }
+    public void setCid(String cid) { this.cid = cid; }
+
+    public Author getAuthor() { return author; }
+    public void setAuthor(Author author) { this.author = author; }
+
+    public Record getRecord() { return record; }
+    public void setRecord(Record record) { this.record = record; }
+
+    public int getReplyCount() { return replyCount; }
+    public void setReplyCount(int replyCount) { this.replyCount = replyCount; }
+
+    public int getRepostCount() { return repostCount; }
+    public void setRepostCount(int repostCount) { this.repostCount = repostCount; }
+
+    public int getLikeCount() { return likeCount; }
+    public void setLikeCount(int likeCount) { this.likeCount = likeCount; }
+
+    public int getQuoteCount() { return quoteCount; }
+    public void setQuoteCount(int quoteCount) { this.quoteCount = quoteCount; }
+
+    public String getIndexedAt() { return indexedAt; }
+    public void setIndexedAt(String indexedAt) { this.indexedAt = indexedAt; }
+
+    public List<Post> getReplies() { return replies; }
+    public void setReplies(List<Post> replies) { this.replies = replies; }
+
+    public void setContent(String content) {
+        this.postContent = content;
+    }
+
+    public String getContent() {
+        return postContent;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
