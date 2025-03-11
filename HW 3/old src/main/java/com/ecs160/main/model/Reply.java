015d0299faed62a329f941c2e0afc9177e2296d1
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-11 01:31:48 -0700

    old files

---
+++ HW 3/old src/main/java/com/ecs160/main/model/Reply.java
@@ -0,0 +1,15 @@
+package com.ecs160.main.model;
+
+public class Reply {
+    private String text;
+
+    public Reply() {}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+} 
