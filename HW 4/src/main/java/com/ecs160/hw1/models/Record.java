4fbf60f35bc1b7b9161f2a8d7e9c4e81e230258b
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-21 17:08:02 -0700

    old models from hw1

---
+++ HW 4/src/main/java/com/ecs160/hw1/models/Record.java
@@ -0,0 +1,52 @@
+package com.ecs160.hw1.models;
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class Record {
+    // vals
+    private String createdAt;
+    private List<String> langs;
+    private String text;
+    private String author;
+    private double weight;
+    private LocalDateTime timestamp;
+    private int wordCount;
+
+    // getters and setters
+    public String getCreatedAt() { return createdAt; }
+    public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
+
+    public List<String> getLangs() { return langs; }
+    public void setLangs(List<String> langs) { this.langs = langs; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
