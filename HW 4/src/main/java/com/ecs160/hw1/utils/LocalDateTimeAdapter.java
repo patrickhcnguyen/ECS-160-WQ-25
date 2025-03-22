a5b85993a306baaab4d1126535a0197688ca3a42
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-21 17:11:17 -0700

    util funcs

---
+++ HW 4/src/main/java/com/ecs160/hw1/utils/LocalDateTimeAdapter.java
@@ -0,0 +1,21 @@
+package com.ecs160.hw1.utils;
+
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+    @Override
+    public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(formatter.format(src));
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDateTime.parse(json.getAsString(), formatter);
+    }
+}
